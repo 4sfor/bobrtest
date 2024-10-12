1b31f1b8b91fa47ce00d4c630c519e8ee0b80c25
me <siaalex03@gmail.com>
2024-10-13 00:36:18 +0400

    upgrade axios request

--- bot/index.js
+++ bot/index.js
@@ -10,7 +10,7 @@ bot.on('message', async (msg) => {
     const chaId = msg.chat.id;
     console.log(`${new Date()} get message ${msg.text} from ${chaId}`);
     if(msg.text==='/start') {
-        bot.sendMessage(chaId, 'Привет, я бот который поможет тебе узнать погоду! Просто введи названия города');
+        bot.sendMessage(chaId, 'Привет, я бот который поможет тебе узнать погоду! Просто введи название города');
     } else {
         console.log(msg.text);
         const responce = await getWeather(msg.text);
@@ -25,7 +25,13 @@ bot.on('message', async (msg) => {
 
 async function getWeather(city){
     try{
-        const dataWeather = await axios.get(`${process.env.API_WEATHER}?q=${city}&appid=${process.env.API_WEATHER_KEY}&lang=${process.env.API_WEATHER_RESPONCE_LANG}`);
+        const dataWeather = await axios.get(`${process.env.API_WEATHER}`, {params:{
+            q:city,
+            appid:process.env.API_WEATHER_KEY,
+            lang:process.env.API_WEATHER_RESPONCE_LANG,
+        },
+        timeout: 5000,
+    })
         return `${city} - ${(dataWeather.data.weather[0].description)}\nТемпература: ${(dataWeather.data.main.temp-273.15).toFixed(2)}С°\nВлажность: ${dataWeather.data.main.humidity}%`;
     } catch(err){
         console.log(err);
